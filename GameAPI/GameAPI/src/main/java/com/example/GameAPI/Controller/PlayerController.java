546321f4509516b3bca574d8efb70530160fb7e4
Saddiqq <9adi8.tm@gmail.com>
2023-07-18 15:04:07 +0400

    modified a PlayerController.java Class

--- GameAPI/GameAPI/src/main/java/com/example/GameAPI/Controller/PlayerController.java
+++ GameAPI/GameAPI/src/main/java/com/example/GameAPI/Controller/PlayerController.java
@@ -1,18 +1,54 @@
 package com.example.GameAPI.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.GameAPI.Models.Player;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 @RestController
 @RequestMapping(path = "/api/v1/player")
+@CrossOrigin("*")
 public class PlayerController {
 
-@PostMapping
-    public Player  getHomePathMessage() {
-        return "MY HOME PAGE";
+    CopyOnWriteArrayList<Player> listOfPlayers = new CopyOnWriteArrayList<>();
+
+    // Endpoint for creating a new player
+    @PostMapping
+    public Player creatPlayer(@RequestBody Player incomingPlayer){
+        listOfPlayers.add(incomingPlayer);
+        return incomingPlayer;
     }
 
+    // Endpoint for retrieving all players
+    @GetMapping
+    public List<Player> getAllPLayers(){
+        return listOfPlayers;
+    }
 
+    // Endpoint for retrieving a specific player by ID
+    @GetMapping(path = "/{id}")
+    public Player getSpecificPlayer(@PathVariable String id){
+        Player existingPlayer = listOfPlayers.stream().filter(
+                (currPlayer) -> {
+                    return currPlayer.id.equals(id);
+                }
+        ).findFirst().get();
+        return existingPlayer;
+    }
+
+    // Endpoint for updating a specific player by ID
+    @PutMapping(path = "/{id}")
+    public Player updateSpecificPlayer(@PathVariable String id, @RequestBody Player incomingPlayer){
+        Player existingPlayer = getSpecificPlayer(id);
+        existingPlayer.name = incomingPlayer.name;
+        return existingPlayer;
+    }
+
+    // Endpoint for removing a specific player by ID
+    @DeleteMapping(path = "/{id}")
+    public Player removePlayer(@PathVariable String id){
+        Player existingPlayer = getSpecificPlayer(id);
+        listOfPlayers.remove(existingPlayer);
+        return existingPlayer;
+    }
 }
