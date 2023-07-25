f85937630daedfcc65f8e013d1ff980788b0e479
Saddiqq <9adi8.tm@gmail.com>
2023-07-25 10:19:58 +0400

    added a functional update and delete function.

--- GameAPI/GameAPI/src/main/resources/static/script.js
+++ GameAPI/GameAPI/src/main/resources/static/script.js
@@ -20,6 +20,9 @@ function createPlayerCard(player) {
   const playerCard = document.createElement('div');
   playerCard.classList.add('player-card');
 
+  // Add data-player-id attribute to the card
+  playerCard.setAttribute('data-player-id', player.id);
+
   const id = document.createElement('p');
   id.textContent = `ID: ${player.id}`;
 
@@ -28,18 +31,62 @@ function createPlayerCard(player) {
 
   const score = document.createElement('p');
   score.textContent = `Score: ${player.score}`;
+  score.setAttribute('data-score', player.score); // Add data-score attribute to the score element
 
   const deleteButton = document.createElement('button');
   deleteButton.textContent = 'Delete';
   deleteButton.addEventListener('click', () => deletePlayer(player.id));
 
+  const updateForm = document.createElement('form');
+  const updateInput = document.createElement('input');
+  updateInput.type = 'number';
+  updateInput.placeholder = 'Enter new score';
+  const updateButton = document.createElement('button');
+  updateButton.textContent = 'Update Score';
+  updateButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    const updatedScore = updateInput.value;
+    updatePlayer(player.id, updatedScore);
+  });
+
+  updateForm.appendChild(updateInput);
+  updateForm.appendChild(updateButton);
+
   playerCard.appendChild(id);
   playerCard.appendChild(name);
   playerCard.appendChild(score);
   playerCard.appendChild(deleteButton);
+  playerCard.appendChild(updateForm); // Append the update form here
 
   return playerCard;
 }
+
+
+// Update a player's score
+function updatePlayer(playerId, newScore) {
+  const scoreElement = document.querySelector(`[data-player-id="${playerId}"] [data-score]`);
+  if (!scoreElement) {
+    console.error('Error updating player score: Score element not found');
+    return;
+  }
+
+  fetch(`http://localhost:8080/api/v1/player/${playerId}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ score: parseInt(newScore) }),
+  })
+    .then(() => {
+      // Update the displayed score on the card
+      scoreElement.textContent = `Score: ${newScore}`;
+    })
+    .catch((error) => {
+      console.error('Error updating player score:', error);
+    });
+}
+
+
 
 // Delete a player
 function deletePlayer(playerId) {
